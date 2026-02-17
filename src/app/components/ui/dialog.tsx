dfd02a7e3f3dc0caf3306cd94dcb33ee50daff00
Maximilien COMLAN <maximiliencomlan05@gmail.com>
2026-02-18 00:28:35 +0100

    updated animated dialog

--- src/app/components/ui/dialog.tsx
+++ src/app/components/ui/dialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as React from "react"
+import { Activity, ComponentProps, createContext, useContext, useEffect, useState } from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { XIcon } from "lucide-react"
 
@@ -8,15 +8,16 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { motion, AnimatePresence, HTMLMotionProps, Variants } from "motion/react"
 import { Portal } from "../portal";
+import { useDebounce } from "@/hooks/useDebounce";
 
-interface DialogContextProps extends React.ComponentProps<typeof DialogPrimitive.Root> {
+interface DialogContextProps extends ComponentProps<typeof DialogPrimitive.Root> {
 
 }
 
-const DialogContext = React.createContext<DialogContextProps>(null as any);
+const DialogStateContext = createContext<DialogContextProps>(null as any);
 
-const useDialogContext = () => {
-  const value = React.useContext(DialogContext);
+const useDialogState = () => {
+  const value = useContext(DialogStateContext);
   if (!value) {
     throw new Error("useDialogContext must be used within a DialogContext");
   }
@@ -24,11 +25,25 @@ const useDialogContext = () => {
 }
 
 function Dialog({
+  children,
   ...props
 }: React.ComponentProps<typeof DialogPrimitive.Root>) {
-  return <DialogContext.Provider value={{ ...props }}>
-    <DialogPrimitive.Root data-slot="dialog" {...props} />
-  </DialogContext.Provider>
+  const [dialogOpen, setDialogOpen] = useState(props.open || props.defaultOpen);
+
+  useEffect(() => {
+    if (Object.keys(props).includes("open") && props.open !== dialogOpen) {
+      setDialogOpen(props.open);
+    }
+  }, [props.open, dialogOpen]);
+
+  return <DialogStateContext.Provider value={{ open: dialogOpen }}>
+    <DialogPrimitive.Root
+      data-slot="dialog"
+      {...props}
+    >
+      {children}
+    </DialogPrimitive.Root>
+  </DialogStateContext.Provider>
 }
 
 function DialogTrigger({
@@ -65,37 +80,6 @@ function DialogOverlay({
   )
 }
 
-/**
- * 
- * <DialogOverlay className="bg-red-500 fixed w-full" />
-    <AnimatePresence>
-      <motion.div
-        variants={motionVariants}
-        className={cn(
-          "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
-          "bg-background fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 outline-none sm:max-w-lg",
-          className
-        )}
-        key="motion-content"
-        initial={"initial"}
-        animate={"animate"}
-        exit={"exit"}
-        {...props}
-      >
-        {children}
-        {showCloseButton && (
-          <DialogPrimitive.Close
-            data-slot="dialog-close"
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
-          >
-            <XIcon />
-            <span className="sr-only">Close</span>
-          </DialogPrimitive.Close>
-        )}
-      </motion.div>
-    </AnimatePresence>
- */
-
 function DialogContent({
   className,
   children,
@@ -105,82 +89,102 @@ function DialogContent({
   showCloseButton?: boolean
 }) {
 
-  const { open, defaultOpen } = useDialogContext();
+  const { open } = useDialogState();
 
   const overlayVariants: Variants = {
     initial: {
+      opacity: 0,
+      '--blur-sm': '8px',
+      pointerEvents: "none"
+    },
+    animate: {
+      opacity: 1,
+      '--blur-sm': '12px',
+      pointerEvents: "auto"
+    },
+    exit: {
+      opacity: 0,
+      '--blur-sm': '8px',
+      pointerEvents: "none"
+    }
+  }
+
+  const contentVariants: Variants = {
+    initial: {
       scale: .6,
-      opacity: 0
+      opacity: 0,
+      pointerEvents: "none"
     },
     animate: {
       scale: 1,
-      opacity: 1
+      opacity: 1,
+      pointerEvents: "auto"
     },
     exit: {
       scale: .6,
-      opacity: 0
+      opacity: 0,
+      pointerEvents: "none"
     }
   }
 
-  const motionVariants: Variants = {
-    initial: {
-      scale: 60,
-      opacity: 0
-    },
-    animate: {
-      scale: 100,
-      opacity: 1
-    },
-    exit: {
-      scale: 60,
-      opacity: 0
+  const [contentVisible, setContentVisible] = useState(open);
+
+  const { delay } = useDebounce(500);
+
+  useEffect(() => {
+    if (open) {
+      setContentVisible(true);
     }
-  }
+    else {
+      delay(() => {
+        setContentVisible(false);
+      })
+    }
+  }, [open]);
 
   if (!globalThis.document) {
-    return null;
+    return <></>
   }
 
   return (
-    <Portal container={document.body} data-slot="dialog-portal">
+    <Portal container={document.body}>
       <AnimatePresence>
-        {
-          open && <motion.div
-            key="overlay"
-            className="fixed top-0 left-0 right-0 bottom-0 z-50 bg-black/50 backdrop-blur-sm"
-            variants={overlayVariants}
-            initial={"initial"}
-            animate={"animate"}
-            exit={"exit"}
-          >
-          </motion.div>
-        }
-        {
-          open && <motion.div
-            variants={motionVariants}
-            className={cn(
-              "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
-              "bg-background fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 outline-none sm:max-w-lg",
-              className
-            )}
-            key="content"
-            initial={"initial"}
-            animate={"animate"}
-            exit={"exit"}
-            {...props}
-          >
+        <motion.div
+          key="dialog.overlay"
+          className="fixed top-0 left-0 right-0 bottom-0 z-50 bg-black/50 backdrop-blur-sm"
+          variants={overlayVariants}
+          initial="initial"
+          animate={open ? "animate" : "exit"}
+        >
+        </motion.div>
+        <motion.div
+          key="dialog.content"
+          variants={contentVariants}
+          initial="initial"
+          animate={open && contentVisible ? "animate" : "exit"}
+          {...props}
+          layout
+          className={cn(
+            "bg-background fixed top-[50%] left-[50%] z-50",
+            "w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%]",
+            "gap-4 rounded-lg border p-6 shadow-lg outline-none sm:max-w-lg",
+            className
+          )}
+        >
+          <Activity mode={contentVisible ? "visible" : "hidden"}>
             {children}
-            {showCloseButton && (
-              <DialogPrimitive.Close
-                data-slot="dialog-close"
-                className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
-              >
-                <XIcon />
-                <span className="sr-only">Close</span>
-              </DialogPrimitive.Close>
-            )}
-          </motion.div>
-        }
+          </Activity>
+          {showCloseButton && (
+            <DialogPrimitive.Close
+              data-slot="dialog-close"
+              data-state={open ? "open" : "closed"}
+              className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+            >
+              <XIcon />
+              <span className="sr-only">Close</span>
+            </DialogPrimitive.Close>
+          )}
+        </motion.div>
       </AnimatePresence>
     </Portal>
   )
@@ -195,6 +199,7 @@ function DialogHeader({ className, ...props }: React.ComponentProps<"div">) {
     />
   )
 }
+
 
 function DialogFooter({
   className,
@@ -219,6 +224,7 @@ function DialogFooter({
           <Button variant="outline">Close</Button>
         </DialogPrimitive.Close>
       )}
+
     </div>
   )
 }
